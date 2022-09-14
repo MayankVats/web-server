97871eed012c3945ec0d0973af61b61baad46309
MayankVats <vats.2610@gmail.com>
2022-09-14 19:05:44 +0530

    Day end commit - Section 7

--- src/app.js
+++ src/app.js
@@ -1,39 +1,53 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 console.log(__dirname);
 
 const app = express();
-
 const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-// app.get("", (req, res) => {
-//   res.send("<h1>Weather!</h1>");
-// });
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Mayank Vats",
+  });
+});
 
-// app.get("/help", (req, res) => {
-//   res.send([
-//     {
-//       name: "Mayank",
-//       age: 23,
-//     },
-//     {
-//       name: "Nidhi",
-//       age: 23,
-//     },
-//   ]);
-// });
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Mayank Vats",
+  });
+});
 
-// app.get("/about", (req, res) => {
-//   res.send("<h1>About page!</h1>");
-// });
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    message: "This is the help page",
+    name: "Mayank Vats",
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
     forecast: "It is sunny.",
     location: "New Delhi",
   });
+});
+
+app.get("*", (req, res) => {
+  res.send("Error: 404");
 });
 
 app.listen(3000, () => {
